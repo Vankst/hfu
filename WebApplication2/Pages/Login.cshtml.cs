fd1d03f96101f5f201b47ef2fab0631158e097a9
android studio <mobil@NAVIATK.RU>
2022-04-21 11:54:37 +0600

    asda

---
+++ WebApplication2/Pages/Login.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication2.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public LoginModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
